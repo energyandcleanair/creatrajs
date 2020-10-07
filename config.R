03674fab73b9abc6dbbad4a043bb8a59ceaf31b0
Hubert Thieriot <hubert@energyandcleanair.org>
2020-10-07 13:35:33 +0800

    -hystplit_met dir from env

--- config.R
+++ config.R
@@ -14,7 +14,7 @@ ggmap::register_google("AIzaSyAM2hj3VbXCSjAXIjLjLH_DfPPSiV8Bhg0")
 dir_hysplit_output <- "hysplit_output"
 dir.create(dir_hysplit_output, showWarnings = F, recursive = T)
 
-dir_hysplit_met <- file.path("~","development","data","weather","hysplit_met")
+dir_hysplit_met <- Sys.getenv("DIR_HYSPLIT_MET", here::here("hysplit_met"))
 dir.create(dir_hysplit_met, showWarnings = F, recursive = T)
 
 dir_results <- "results"
