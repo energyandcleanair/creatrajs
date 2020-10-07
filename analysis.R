33c845da3746de4a1307f075e037869b9ec3ee25
Hubert Thieriot <ht@erprofiler.com>
2020-10-07 14:56:34 +0800

    Revert "-mettype in caption"
    This reverts commit 22ef0f671a850cbe70a4957091cb6b1643907a08.

--- analysis.R
+++ analysis.R
@@ -41,7 +41,7 @@ mftb.plotted <- mftb %>%
                         country, region_id, paste0(radius_km,"km"), gsub(".","",met_type), sep=".")
 ) %>%
   ungroup() %>%
-  mutate(plot=purrr::pmap_chr(., map.trajs, powerplants=powerplants, met_type=met_type),
+  mutate(plot=purrr::pmap_chr(., map.trajs, powerplants=powerplants),
          meta=purrr::pmap_chr(., utils.save.meta))
 
 
