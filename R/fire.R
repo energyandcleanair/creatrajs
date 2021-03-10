c51bf322e9e964f17c16f93ad59551a553bc2ed1
Hubert Thieriot <hubert@energyandcleanair.org>
2021-03-10 20:11:34 +0800

    -no process_id required

--- R/fire.R
+++ R/fire.R
@@ -202,7 +202,7 @@ fire.attach_to_trajs <- function(mt, buffer_km=10, delay_hour=24){
     left_join(
       mtf %>%
         tidyr::unnest(fires) %>%
-        group_by(location_id, process_id, date) %>%
+        group_by(location_id, date) %>%
         summarise_at(c("fire_frp","fire_count"),
                      mean,
                      na.rm=T) %>%
